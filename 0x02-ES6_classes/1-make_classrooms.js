164b41e06ff7a71a097a460734c8ac7ce158d800
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-03-28 20:13:40 +0100

    debug

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,16 @@
 import ClassRoom from './0-classroom';
 
-/**
- * Creates an array of {@link ClassRoom}s with a specific size.
- * @returns An array of {@link ClassRoom}s.
- */
-export default function initializeRooms() {
-  return [19, 20, 34].map((size) => new ClassRoom(size));
+function initializeRooms() {
+  // Create an array to hold the ClassRoom instances
+  const rooms = [];
+
+  // Create three ClassRoom instances with sizes 19, 20, and 34
+  rooms.push(new ClassRoom(19));
+  rooms.push(new ClassRoom(20));
+  rooms.push(new ClassRoom(34));
+
+  // Return the array of ClassRoom instances
+  return rooms;
 }
+
+export default initializeRooms;
